750e3683e2e41e93fedb8b409466a7b60077a8c6
Jason Livesay <runvnc@gmail.com>
2025-09-11 19:13:51 -0500

    images

--- src/mr_tavily_search/mod.py
+++ src/mr_tavily_search/mod.py
@@ -6,8 +6,8 @@ from lib.providers.services import service
 from lib.providers.commands import command
 import trafilatura
 import nanoid
+from langchain_tavily import TavilySearch
 
-tavily_client = TavilyClient(api_key=os.environ.get('TAVILY_API_KEY'))
 
 @service()
 async def web_search(query, num_results=5):
@@ -21,21 +21,19 @@ async def web_search(query, num_results=5):
         list: A list of dictionaries containing search results.
     """
     try:
-        response = tavily_client.search(query)
-        from langchain_tavily import TavilySearch
 
-    tool = TavilySearch(
-        max_results=num_results,
-        search_depth="advanced",
-        #include_answer=True,
-        #include_raw_content=True,
-        include_images=True,
-        include_image_descriptions=True
-        #response_format="content_and_artifact"
-    )
-    id = nanoid.generate()
-    tool_results = tool.run(tool_input = {"query": query}, tool_call_id = id)
-    results = tool_results.content
+        tool = TavilySearch(
+            max_results=num_results,
+            search_depth="advanced",
+            #include_answer=True,
+            #include_raw_content=True,
+            include_images=True,
+            include_image_descriptions=True
+            #response_format="content_and_artifact"
+        )
+        id = nanoid.generate()
+        tool_results = tool.run(tool_input = {"query": query}, tool_call_id = id)
+        results = tool_results.content
         return results
         #results = response.get('results', [])[:num_results]
         #return [{'title': r['title'], 
